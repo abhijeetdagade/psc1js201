add38886ddb4c9ad5195c2f87319898d0dcf36c8
Abhijeet Dagade <101392865+abhijeetdagade@users.noreply.github.com>
2022-06-26 00:45:29 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,38 @@
+function stoeData(e){
+e.preventDefault();
+let form=document.getElementById("students_data")
+let name=form.name.value;
+let course=form.course.value;
+let unit=form.unit.value;
+let image=form.image.value;
+let batch=form.batch.value;
+let s1=new student(name,course,unit,image,batch);
+let data=JSON.parse(localStorage.getItem("students")) || [];
+data.push(s1);
+localStorage.setItem("students",JSON.stringify(data));
+}
+
+
+function student(n,c,u,i,b){
+    this.name=n;
+    this.course=c;
+    this.unit=u;
+    this.image=i;
+    this.batch=`ft-web${b}`;
+}
+
+function calculate(){
+    let data=JSON.parse(localStorage.getItem("students")) || [];
+var obj={}
+for(var i=0;i<data.length;i++){
+    if(!obj[data[i].batch]){
+        obj[data[i].batch]=0;
+    }
+}
+for(var i=0;i<data.length;i++){
+        obj[data[i].batch]++;
+    }
+
+console.log(obj);
+}
+calculate();
